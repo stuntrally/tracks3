b288645e4dfa17cf6782f837a5d8af449920679f
cryham <cryham@gmail.com>
2024-05-18 02:26:42 +0200

    add previews.py

---
+++ previews.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import os, sys
+from PIL import Image
+
+tdir = '.'
+trks = os.listdir(tdir)
+#size = 128, 128
+size = 256, 256
+
+for trk in trks:
+    if trk.find('-') != -1:
+
+        infile = trk+'/preview/view.jpg'
+        #print(infile)
+        outfile = '_previews/'+trk+'.jpg'
+        try:
+            im = Image.open(infile)
+            im.thumbnail(size, Image.Resampling.LANCZOS)
+            im.save(outfile, "JPEG", quality=95)
+        except IOError:
+            print('cannot create for: '+trk)
